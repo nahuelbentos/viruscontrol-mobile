f33cd06ce056cfb102a565d46da5a6c629bd5a2b
Santiago Chabert <santiagochabert@gmail.com>
2020-06-11 20:17:17 -0300

    Merge login

--- app/src/main/java/com/grupo14/viruscontrol/viruscontroluy/modelos/Sintoma.java
+++ app/src/main/java/com/grupo14/viruscontrol/viruscontroluy/modelos/Sintoma.java
@@ -3,7 +3,28 @@ package com.grupo14.viruscontrol.viruscontroluy.modelos;
 public class Sintoma {
     private String id;
     private String nombre;
-    public Sintoma(){
-        super();
+//    public Sintoma(){
+//        super();
+//    }
+    public Sintoma(String id1, String nombre){
+
+        this.id = id1;
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
